ca1eb5afd7b3284332008970e4974ab8a481ea54
madhankumar028 <madhankumar028@gmail.com>
2021-06-13 10:16:01 +0530

    adds httpclient

---
+++ src/api.js
@@ -0,0 +1,26 @@
+export const HTTPClient = async(endpoint, {body, ...customConfig} = {}) => {
+  const headers = {'Content-Type': 'application/json'};
+  const config = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  return window
+  .fetch(endpoint, config)
+  .then(async response => {
+    if (response.ok) {
+      return await response.json();
+    } else {
+      const errorMessage = await response.text();
+      return Promise.reject(new Error(errorMessage));
+    }
+  })
+};
