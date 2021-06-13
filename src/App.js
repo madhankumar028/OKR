dc553bfc58c0fdcc6ff008785eac67a068cf97d8
madhankumar028 <madhankumar028@gmail.com>
2021-06-13 10:17:49 +0530

    handles the api call to fetch the okr items and render it inside the accordion

--- src/App.js
+++ src/App.js
@@ -2,16 +2,53 @@ import React from 'react';
 
 import { Accordion } from './components/Accordion/Accordion';
 
+import { HTTPClient } from './api';
+
+import './styles/index.scss';
+
 function App() {
+
+  const [parentObjective, setParentObjective] = React.useState([]);
+  const [childObjective, setChildObjective] = React.useState([]);
+
+  const formatOKRList = (OKRList) => {
+    setParentObjective(OKRList.filter(OKRItem => !OKRItem.parent_objective_id));
+    setChildObjective(OKRList.filter(OKRItem => OKRItem.parent_objective_id));
+  };
+
+  const fetchOKRList = async () => {
+    const { data } = await HTTPClient(`/database/db.json`);
+    formatOKRList(data);
+  };
+
+  React.useEffect(() => {
+    fetchOKRList();
+  }, []);
+
   return (
-    <div className="app">
+    <main className="okr__app--wrapper">
       <header className="header">
-        <p data-testid="okr__app--test" className="okr__app--wrapper">
-          OKR APP
-        </p>
-        <Accordion title="test" shouldOpen></Accordion>
+        <p data-testid="okr__app--test">OKR APP</p>
       </header>
-    </div>
+      <section>
+        {
+          parentObjective.length > 0 && (
+            parentObjective.map((objective, index) => (
+              <Accordion title={`${index + 1}. ${objective.title}`} shouldOpen={index === 0} key={objective.id}>
+                <ol className="okr__list--wrapper">
+                  {
+                    childObjective.filter(item => item.parent_objective_id === objective.id)
+                    .map(item => (
+                      <li type="a" className="heading-sm-m" key={item.id}>{item.title}</li>
+                    ))
+                  }
+                </ol>
+              </Accordion>
+            ))
+          )
+        }
+      </section>
+    </main>
   );
 }
 
